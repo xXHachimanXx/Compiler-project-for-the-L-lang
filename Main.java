64b2a9c1c83d67e32e16c9f18b5ca69c720eed5a
axell-brendow <breno.axel@gmail.com>
2021-03-16 08:24:20 -0300

    Start Lexer class with all keywords

--- Main.java
+++ Main.java
@@ -1,3 +1,6 @@
+import java.io.PushbackReader;
+import java.util.HashMap;
+
 enum TokenType {
     EOF,
     MAIN,
@@ -58,5 +61,40 @@ class Token {
     Token(TokenType type, char c) {
         this(type, "" + c);
     }
+}
+
+class Lexer {
+    public int line;
+    public PushbackReader reader;
+    public HashMap<String, Token> keywords;
+
+    void initKeywords() {
+        keywords = new HashMap<>();
+        keywords.put("main", new Token(TokenType.MAIN, "main"));
+        keywords.put("write", new Token(TokenType.WRITE, "write"));
+        keywords.put("writeln", new Token(TokenType.WRITELN, "writeln"));
+        keywords.put("readln", new Token(TokenType.READLN, "readln"));
+        keywords.put("return", new Token(TokenType.RETURN, "return"));
+        keywords.put("if", new Token(TokenType.IF, "if"));
+        keywords.put("then", new Token(TokenType.THEN, "then"));
+        keywords.put("else", new Token(TokenType.ELSE, "else"));
+        keywords.put("for", new Token(TokenType.FOR, "for"));
+        keywords.put("int", new Token(TokenType.INT, "int"));
+        keywords.put("char", new Token(TokenType.CHAR, "char"));
+        keywords.put("boolean", new Token(TokenType.BOOLEAN, "boolean"));
+        keywords.put("final", new Token(TokenType.FINAL, "final"));
+        keywords.put("TRUE", new Token(TokenType.TRUE, "TRUE"));
+        keywords.put("FALSE", new Token(TokenType.FALSE, "FALSE"));
+        keywords.put("or", new Token(TokenType.OR, "or"));
+        keywords.put("and", new Token(TokenType.AND, "and"));
+        keywords.put("not", new Token(TokenType.NOT, "not"));
+    }
+
+    Lexer(PushbackReader reader) {
+        this.line = 1;
+        this.reader = reader;
+        initKeywords();
+    }
+
 }
 
